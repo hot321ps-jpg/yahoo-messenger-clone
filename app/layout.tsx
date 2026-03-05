64bb2b68283f67760fefdc097bec77c85ba65756
hot321ps-jpg <hot321ps@gmail.com>
2026-03-05 20:52:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,17 @@ export default function RootLayout({
   children
 }:{
   children:React.ReactNode
-}) {
-  return (
-    <html lang="zh-Hant">
-      <body className="bg-[#2b0052] text-white">
-        {children}
-      </body>
-    </html>
+}){
+
+  return(
+
+  <html lang="zh-Hant">
+    <body className="bg-[#2b0052] text-white">
+
+      {children}
+
+    </body>
+  </html>
+
   )
 }
