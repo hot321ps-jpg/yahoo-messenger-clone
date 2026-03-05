f9b6e1c490f48121320bf3b1645a30128ea95d53
hot321ps-jpg <hot321ps@gmail.com>
2026-03-05 20:53:08 +0800

    Update FriendList.tsx

--- components/FriendList.tsx
+++ components/FriendList.tsx
@@ -5,34 +5,43 @@ const friends = [
 ];
 
 export default function FriendList({
-  selected,
-  setSelected,
-}: {
-  selected: string;
-  setSelected: (name: string) => void;
-}) {
-  return (
-    <div className="w-64 bg-purple-800">
+selected,
+setSelected
+}:{
+selected:string
+setSelected:(name:string)=>void
+}){
 
-      <div className="p-4 font-bold">
-        Yahoo Messenger
-      </div>
+return(
 
-      {friends.map((f) => (
-        <div
-          key={f.name}
-          onClick={() => setSelected(f.name)}
-          className="p-3 cursor-pointer hover:bg-purple-700 flex justify-between"
-        >
-          <span>{f.name}</span>
+<div className="w-64 bg-purple-800">
 
-          <span
-            className={`w-3 h-3 rounded-full ${
-              f.online ? "bg-green-400" : "bg-gray-400"
-            }`}
-          />
-        </div>
-      ))}
-    </div>
-  );
+<div className="p-4 font-bold text-lg">
+Yahoo Messenger
+</div>
+
+{friends.map((f)=>(
+
+<div
+key={f.name}
+onClick={()=>setSelected(f.name)}
+className="p-3 cursor-pointer hover:bg-purple-700 flex justify-between"
+>
+
+<span>{f.name}</span>
+
+<span
+className={`w-3 h-3 rounded-full ${
+f.online?"bg-green-400":"bg-gray-400"
+}`}
+/>
+
+</div>
+
+))}
+
+</div>
+
+)
+
 }
