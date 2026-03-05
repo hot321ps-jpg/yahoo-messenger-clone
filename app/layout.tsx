09339abbb1dc9e0328f0f564e19c1d8e3495511a
hot321ps-jpg <hot321ps@gmail.com>
2026-03-05 20:43:08 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children
+}:{
+  children:React.ReactNode
+}) {
+  return (
+    <html lang="zh-Hant">
+      <body className="bg-[#2b0052] text-white">
+        {children}
+      </body>
+    </html>
+  )
+}
